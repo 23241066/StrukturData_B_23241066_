677c0daadce99674f4d353dad353ce20c4caf248
MSyakirArridho <146050299+23241066@users.noreply.github.com>
2024-04-01 11:59:32 +0800

    1 april 2024

---
+++ PYTHON/meet2.py
@@ -0,0 +1,25 @@
+# Belajar variabel
+#assigment
+x = 3
+y = 9
+uang = 2000
+
+
+# pemanggilan
+print ("Nilai Uas saya =", x )
+print ("Nilai Uts saya =", y )
+print ("Sisa uang saya =", uang )
+
+# Penamaan variabel
+nilai_A = 87 # tidak boleh spasi
+nilai10 = 79 # tidak boleh awalan angka
+
+# input variabel kedua
+x = 30
+print ("Nilai Uas saya =", x )
+x = 300
+print ("Nilai Uas saya =", x )
+x = 3000
+print ("Nilai Uas saya =", x )
+x = 30000
+print ("Nilai Uas saya =", x )
