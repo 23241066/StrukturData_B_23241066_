894cc1320e2c5583edd7d0e36f6e47f9dbfb20ed
MSyakirArridho <146050299+23241066@users.noreply.github.com>
2024-06-03 11:45:19 +0800

    senin 03/06/2024

---
+++ PYTHON/meet9.py
@@ -0,0 +1,27 @@
+# percabangan jika maka
+
+name = input("what is your name : ")
+
+
+# if kondisi : aksi
+
+# if standar
+if name == "edy" : print("Dosen ganteng")
+print("end of program")
+
+# if indentasi
+name = input("what is your name : ")
+if name == "edy" :
+    print("dosen ganteng")
+    print("ganteng banget")
+    print("kece sekali")
+print("end of program")
+
+# IF ELSE 
+name = input("what is your name : ")
+if name == "edy":
+    print("dosen ganteng")
+    print(f"terimakasih {name}")
+else:
+    print("anda tidak ganteng")
+print("end of program")   
