211154c10d41c46e36f3aeb05bafdb37a95a932c
MSyakirArridho <146050299+23241066@users.noreply.github.com>
2024-06-22 10:01:48 +0800

    Add files via upload

---
+++ PYTHON/meet10.py
@@ -0,0 +1,39 @@
+# belajar perbandingan lanjutan, ELIF
+
+nama = input("masukan nama :")
+if nama == "edy" :  # kondisi 1
+    print("dosen ganteng")  # aksi true 1
+elif nama == "haryanto" :  # kondisi 2
+    print("dosen ganteng banget")  # aksi true 2
+elif nama == "ika" :  # kondisi 3
+    print("anda kan mahasiswi")  # aksi true 3
+    
+else:
+    print("bukan dosen ganteng")  # aksi false
+print("program selesai")
+
+
+# percabangan ELIF
+
+nama = input("masukan nama :")
+if nama == "chelly" :
+    print("mahasiswa rajin")
+elif nama == "adam" :
+    print("mahasiswa rajin")
+elif nama == "budi" :
+    print("mahasiswa rajin")
+elif nama == "bima" :
+    print("mahasiswa rajin")
+elif nama == "azzam" :
+    print("mahasiswa rajin")
+elif nama == "ridho" :
+    print("mahasiswa rajin")
+elif nama == "ilham" :
+    print("mahasiswa rajin")
+elif nama == "abdul" :
+    print("mahasiswa rajin")
+    
+else :
+    print("Mahasiswa tidak rajin")
+print("program selesai")
+    
